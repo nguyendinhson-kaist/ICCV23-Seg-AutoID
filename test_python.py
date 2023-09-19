184be2e50255b237d749375f6c684e491c84357b
Nguyen Dinh Son <117170987+nguyendinhson-kaist@users.noreply.github.com>
2023-09-20 00:48:21 +0900

    Minor fix

--- test_python.py
+++
@@ -1,9 +0,0 @@
-from torchvision.models.detection.mask_rcnn import maskrcnn_resnet50_fpn
-import torch
-
-
-x = torch.rand(1,3,1920,1080)
-model = maskrcnn_resnet50_fpn()
-print(model)
-model.eval()
-print(model(x))
